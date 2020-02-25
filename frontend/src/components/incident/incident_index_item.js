51134d58c420999c1b237d2590387c031b71f931
Shawki <shawksta64@gmail.com>
2020-02-25 14:45:32 -0500

    Made the skeleton and a general index but im gonna need confirmation before i move on to Item

---
+++ frontend/src/components/incident/incident_index_item.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class IndexItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default withRouter(IndexItem);
