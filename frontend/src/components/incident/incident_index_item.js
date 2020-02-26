7b24bc2e0d9d830215597345fd8eb21fc0aba292
Shawki <shawksta64@gmail.com>
2020-02-26 15:55:57 -0500

    Got basic styling, chris will take over eventually, going to start making seeds and realistic lat and lng

--- frontend/src/components/incident/incident_index_item.js
+++ frontend/src/components/incident/incident_index_item.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import "./incident.css";
 
 class IndexItem extends React.Component {
     render() {
         return(
             <div className="incident-container" >
-                <p className="latitude">Lat: {this.props.incident.lat}</p>
-                <p className="longitude">Lng: {this.props.incident.lng}</p>
-                <p className="category">Category: {this.props.incident.category}</p>
-                <p className="time">Time: {this.props.incident.time}</p>
+                <div className="direction"> 
+                    <p className="latitude">Lat: {this.props.incident.lat}</p>
+                    <p className="longitude">Lng: {this.props.incident.lng}</p>
+                </div>
+                <div className="cat-time" >
+                    <p className="category">Category: {this.props.incident.category}</p>
+                    <p className="time">Time: {this.props.incident.time}</p>
+                </div>
                 <p className="description">Description: {this.props.incident.description}</p>
             </div>
         );
--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import IncidentIndex from '../incident/incident_index';
+import '../incident/incident.css';
 
 class Profile extends React.Component {
     // constructor(props) {
