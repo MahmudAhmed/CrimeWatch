24d0a7ff38f612bc60df7fb39edd0cd0256e1ec3
MahmudAhmed <moe.purplefox@gmail.com>
2020-02-27 10:11:30 -0500

    public to build

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/public"));
+  app.use(express.static("frontend/build"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 } 
 
