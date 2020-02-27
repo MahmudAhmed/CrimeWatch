459e39111a537b2cf1fcebca548372ae1c31cc1d
MahmudAhmed <moe.purplefox@gmail.com>
2020-02-27 11:01:31 -0500

    line13

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const passport = require("passport");
 const path = require("path");
 
 // mongoose.set("runValidators", true);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  // app.use("/", express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} 
 debugger
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,7 +24,7 @@ mongoose
   .catch(err => console.log(err));
 
 
-app.get("/", (req, res) => res.send("Welcome to CrimeWatch!!!"));
+// app.get("/", (req, res) => res.send("Welcome to CrimeWatch!!!"));
 
 app.use(passport.initialize());
 require("./config/passport")(passport);
@@ -31,9 +39,3 @@ app.use("/api/incidents", incidents);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-} 
