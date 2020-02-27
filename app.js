29e6b3c232fce7718ff00e5b90f6d30c25840e2b
MahmudAhmed <moe.purplefox@gmail.com>
2020-02-27 10:52:09 -0500

    env to bottom

--- app.js
+++ app.js
@@ -21,12 +21,6 @@ app.get("/", (req, res) => res.send("Welcome to CrimeWatch!!!"));
 app.use(passport.initialize());
 require("./config/passport")(passport);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-} 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -36,3 +30,10 @@ app.use("/api/incidents", incidents);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} 
