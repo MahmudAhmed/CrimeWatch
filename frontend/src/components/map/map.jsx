c684ba07ef8e8f9f2c99ab2521248f512e5d2b8a
MahmudAhmed <moe.purplefox@gmail.com>
2020-03-11 20:52:28 -0400

    funtional

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import keys from "../../config/keys";
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class CrimeMap extends Component {
-
+  
   render() {
+    const renderMarkers = (map, maps) => {
+      let marker = new maps.Marker({
+        position: { lat: 40.6924415, lng: -73.9892479 },
+        map,
+        title: 'Hello World!'
+      });
+      return marker;
+    };
     return (
       <div style={{ height: "100vh", width: "100%" }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: keys.maps }}
           defaultCenter={{ lat: 40.6924415, lng: -73.9892479 }}
           defaultZoom={13}
+          onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
         >
-
         </GoogleMapReact>
       </div>
     );
   }
+}
+
+const Marker = props => {
+  return <div className="SuperAwesomePin"></div>
 }
 
 export default CrimeMap;
